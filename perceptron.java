0b27b77708e42a49e935ac621f9551ef34d3b348
Kenny Collins <kennethcollins@sandiego.edu>
2024-03-02 15:59:37 -0800

    frogot to push this

--- perceptron.java
+++ perceptron.java
@@ -1,3 +1,24 @@
 public class perceptron {
+
+    public void train(String trainingDataFile, int weightInit, int maxEpochs, String weightSettingsFile, double alpha,
+            double theta, double threshold) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'train'");
+    }
+
+    public String getEpochs() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getEpochs'");
+    }
+
+    public void test(String testingDataFile, String resultsFile) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'test'");
+    }
+
+    public void loadWeights(String testingDataFile) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'loadWeights'");
+    }
     
 }
