a50d51b40c441da8f2da48e3fcacf36271c2d3bc
Kenny Collins <kennethcollins@sandiego.edu>
2024-03-04 20:43:41 -0800

    Converging super slow

--- perceptron.java
+++ perceptron.java
@@ -79,6 +79,8 @@ public class Perceptron {
                             weights.set(i, weights.get(i) + alpha * (target - y_j) * activations.get(i));
                         }
                         stoppingCondition = false;  
+                    } else {
+                        break;
                     }
                 }
             }
@@ -121,63 +123,3 @@ public class Perceptron {
     }
 
 }
-
-
-// public class Perceptron {
-//     public ArrayList<Double> weights;
-//     public ArrayList<Double> biases;
-//     public boolean stoppingCondition;
-
-//     public Perceptron(int inputSize, int outputSize) {
-//         weights = new ArrayList<>(inputSize);
-//         biases = new ArrayList<>(outputSize);
-//         initializeWeightsAndBiases();
-//         stoppingCondition = false;
-//     }
-
-
-//     public void train(ArrayList<ArrayList<Integer> trainingSet, ArrayList<Integer> targetSet, double theta) {
-        // while (!stoppingCondition) {
-        //     stoppingCondition = true;  
-
-        //     for (int k = 0; k < trainingSet.size(); k++) {
-        //         ArrayList<Integer> input = trainingSet.get(k);
-        //         int target = targetSet.get(k);
-
-        //         //  set activation of each input unit
-        //         ArrayList<Double> activations = new ArrayList<>(input);
-
-        //         // compute activation of each output unit
-        //         for (int j = 0; j < biases.size(); j++) {
-        //             double y_in_j = biases.get(j);
-
-        //             for (int i = 0; i < weights.size(); i++) {
-        //                 y_in_j += input.get(i) * weights.get(i);
-        //             }
-
-        //             // activation function
-        //             int y_j;
-        //             if (y_in_j > theta) {
-        //                 y_j = 1;
-        //             } else if (-theta <= y_in_j && y_in_j <= theta) {
-        //                 y_j = 0;
-        //             } else {
-        //                 y_j = -1;
-        //             }
-
-        //             // update biases and weights
-        //             if (target != y_j) {
-        //                 biases.set(j, biases.get(j) + target);
-        //                 for (int i = 0; i < weights.size(); i++) {
-        //                     weights.set(i, weights.get(i) + target * input.get(i));
-        //                 }
-        //                 stoppingCondition = false;  
-        //             }
-        //         }
-        //     }
-        // }
-//     }
-// }
-
-
-
